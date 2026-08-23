a1b5c20ea50822af55eb7eea19007a782b058844
cantidokun <thchumour@gmail.com>
2026-08-22 20:45:57 -0400

    feat: add Niagara transport declarations

--- unreal/AtlasUnrealHarness/Source/AtlasUnrealTransport/Public/AtlasTransportServer.h
+++ unreal/AtlasUnrealHarness/Source/AtlasUnrealTransport/Public/AtlasTransportServer.h
@@ -12,10 +12,8 @@ class FAtlasTransportServer : public FRunnable
 public:
     FAtlasTransportServer();
     virtual ~FAtlasTransportServer();
-
     bool StartServer();
     void StopServer();
-
     virtual bool Init() override;
     virtual uint32 Run() override;
     virtual void Stop() override;
@@ -24,70 +22,27 @@ public:
 private:
     static const FString PipeName;
     static const int32 MaxMessageSize;
-
     FRunnableThread* Thread;
     FThreadSafeBool bStopRequested;
     void* PipeHandle;
-
-    struct FTransportRequest
-    {
-        FString RequestId;
-        FString OperationName;
-        FString Capability;
-        FString Kind;
-        TSharedPtr<FJsonObject> Arguments;
-        TArray<FString> EntityIds;
-        FString AuthorizationId;
+    struct FTransportRequest { FString RequestId; FString OperationName; FString Capability; FString Kind; TSharedPtr<FJsonObject> Arguments; TArray<FString> EntityIds; FString AuthorizationId; };
+    struct FTransportResponse { FString RequestId; FString OperationName; TArray<FString> EntityIds; bool bSuccess; TSharedPtr<FJsonObject> ObservedState; FString Error; FString Source; };
+    struct FGameThreadExecutionState {
+        FTransportRequest Request; FTransportResponse Response; FString Error; TSharedPtr<FJsonObject> ObservedState; FThreadSafeBool bCompleted; FThreadSafeBool bSuccess; FThreadSafeBool bCancelled; FEvent* CompletionEvent;
+        FGameThreadExecutionState() : bCompleted(false), bSuccess(false), bCancelled(false), CompletionEvent(FPlatformProcess::GetSynchEventFromPool(false)) {}
+        ~FGameThreadExecutionState() { if (CompletionEvent) FPlatformProcess::ReturnSynchEventToPool(CompletionEvent); }
     };
-
-    struct FTransportResponse
-    {
-        FString RequestId;
-        FString OperationName;
-        TArray<FString> EntityIds;
-        bool bSuccess;
-        TSharedPtr<FJsonObject> ObservedState;
-        FString Error;
-        FString Source;
-    };
-
-    struct FGameThreadExecutionState
-    {
-        FTransportRequest Request;
-        FTransportResponse Response;
-        FString Error;
-        TSharedPtr<FJsonObject> ObservedState;
-        FThreadSafeBool bCompleted;
-        FThreadSafeBool bSuccess;
-        FThreadSafeBool bCancelled;
-        FEvent* CompletionEvent;
-
-        FGameThreadExecutionState()
-            : bCompleted(false), bSuccess(false), bCancelled(false), CompletionEvent(FPlatformProcess::GetSynchEventFromPool(false)) {}
-
-        ~FGameThreadExecutionState()
-        {
-            if (CompletionEvent) FPlatformProcess::ReturnSynchEventToPool(CompletionEvent);
-        }
-    };
-
-    bool CreatePipeHandle();
-    void CloseNamedPipe();
-    bool WaitForClient();
-    bool ReadRequest(FString& OutJsonRequest);
-    bool WriteResponse(const FString& JsonResponse);
-    bool ParseRequest(const FString& JsonString, FTransportRequest& OutRequest);
-    FString SerializeResponse(const FTransportResponse& Response);
-    bool ValidateRequest(const FTransportRequest& Request, FString& OutError);
-    bool ExecuteRequest(const FTransportRequest& Request, FTransportResponse& OutResponse);
-
+    bool CreatePipeHandle(); void CloseNamedPipe(); bool WaitForClient(); bool ReadRequest(FString& OutJsonRequest); bool WriteResponse(const FString& JsonResponse); bool ParseRequest(const FString& JsonString,FTransportRequest& OutRequest); FString SerializeResponse(const FTransportResponse& Response); bool ValidateRequest(const FTransportRequest& Request,FString& OutError); bool ExecuteRequest(const FTransportRequest& Request,FTransportResponse& OutResponse);
     static void ExecuteOnGameThread(TSharedPtr<FGameThreadExecutionState> SharedState);
-    static bool InspectTargetActors(const TArray<FString>& EntityIds, TSharedPtr<FJsonObject>& OutObservedState, FString& OutError);
-    static bool SetActorLocation(const FTransportRequest& Request, TSharedPtr<FJsonObject>& OutObservedState, FString& OutError);
-    static bool SetActorRotation(const FTransportRequest& Request, TSharedPtr<FJsonObject>& OutObservedState, FString& OutError);
-    static bool SetActorScale(const FTransportRequest& Request, TSharedPtr<FJsonObject>& OutObservedState, FString& OutError);
-    static bool InspectMaterialState(const TArray<FString>& EntityIds, TSharedPtr<FJsonObject>& OutObservedState, FString& OutError);
-    static bool ApplyMaterialVariant(const FTransportRequest& Request, TSharedPtr<FJsonObject>& OutObservedState, FString& OutError);
-    static bool BuildMaterialVariantState(AActor* Actor, TSharedPtr<FJsonObject>& OutMaterialState, FString& OutError);
+    static bool InspectTargetActors(const TArray<FString>& EntityIds,TSharedPtr<FJsonObject>& OutObservedState,FString& OutError);
+    static bool SetActorLocation(const FTransportRequest& Request,TSharedPtr<FJsonObject>& OutObservedState,FString& OutError);
+    static bool SetActorRotation(const FTransportRequest& Request,TSharedPtr<FJsonObject>& OutObservedState,FString& OutError);
+    static bool SetActorScale(const FTransportRequest& Request,TSharedPtr<FJsonObject>& OutObservedState,FString& OutError);
+    static bool InspectMaterialState(const TArray<FString>& EntityIds,TSharedPtr<FJsonObject>& OutObservedState,FString& OutError);
+    static bool ApplyMaterialVariant(const FTransportRequest& Request,TSharedPtr<FJsonObject>& OutObservedState,FString& OutError);
+    static bool BuildMaterialVariantState(AActor* Actor,TSharedPtr<FJsonObject>& OutMaterialState,FString& OutError);
+    static bool InspectNiagaraState(const TArray<FString>& EntityIds,TSharedPtr<FJsonObject>& OutObservedState,FString& OutError);
+    static bool ApplyNiagaraVariant(const FTransportRequest& Request,TSharedPtr<FJsonObject>& OutObservedState,FString& OutError);
+    static bool BuildNiagaraVariantState(AActor* Actor,TSharedPtr<FJsonObject>& OutNiagaraState,FString& OutError);
     static AActor* FindActorByEntityId(const FString& EntityId);
 };
