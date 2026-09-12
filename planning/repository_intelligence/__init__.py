9fea8f376e21074d36ead904e8835fa466c1d84f
cantidokun <thchumour@gmail.com>
2026-09-12 14:22:52 -0400

    M13.5: export context benchmark APIs

--- planning/repository_intelligence/__init__.py
+++ planning/repository_intelligence/__init__.py
@@ -1,6 +1,22 @@
 """Deterministic repository intelligence for Atlas development tooling."""
 
+from planning.repository_intelligence.benchmark import (
+    ContextBenchmarkCase,
+    ContextBenchmarkResult,
+    compile_benchmark_context,
+    curated_context_benchmark_cases,
+    run_context_benchmark,
+    validate_benchmark_corpus,
+    validate_benchmark_inputs,
+)
 from planning.repository_intelligence.context import ContextFile, ContextPackage, compile_context
+from planning.repository_intelligence.evaluation import (
+    ContextEvaluationCase,
+    ContextEvaluationResult,
+    aggregate_evaluations,
+    evaluate_context,
+    repeat_context,
+)
 from planning.repository_intelligence.index import (
     GitHistory,
     ImportRecord,
@@ -18,6 +34,10 @@ from planning.repository_intelligence.relevance import (
 )
 
 __all__ = [
+    "ContextBenchmarkCase",
+    "ContextBenchmarkResult",
+    "ContextEvaluationCase",
+    "ContextEvaluationResult",
     "ContextFile",
     "ContextPackage",
     "ContextRoutingAdvice",
@@ -30,7 +50,15 @@ __all__ = [
     "RepositoryIndex",
     "SymbolRecord",
     "advise_context_route",
+    "aggregate_evaluations",
     "build_repository_index",
+    "compile_benchmark_context",
     "compile_context",
+    "curated_context_benchmark_cases",
+    "evaluate_context",
     "rank_repository_files",
+    "repeat_context",
+    "run_context_benchmark",
+    "validate_benchmark_corpus",
+    "validate_benchmark_inputs",
 ]
