5d47735bb615e35fd27724bd79b84f2dfb034dc2
cantidokun <thchumour@gmail.com>
2026-09-12 14:14:09 -0400

    M13.3: export repository intelligence context APIs

--- planning/repository_intelligence/__init__.py
+++ planning/repository_intelligence/__init__.py
@@ -1,5 +1,6 @@
 """Deterministic repository intelligence for Atlas development tooling."""
 
+from planning.repository_intelligence.context import ContextFile, ContextPackage, compile_context
 from planning.repository_intelligence.index import (
     GitHistory,
     ImportRecord,
@@ -7,11 +8,26 @@ from planning.repository_intelligence.index import (
     SymbolRecord,
     build_repository_index,
 )
+from planning.repository_intelligence.relevance import (
+    RelevanceExplanation,
+    RelevanceQuery,
+    RelevanceResult,
+    RelevanceWeights,
+    rank_repository_files,
+)
 
 __all__ = [
+    "ContextFile",
+    "ContextPackage",
     "GitHistory",
     "ImportRecord",
+    "RelevanceExplanation",
+    "RelevanceQuery",
+    "RelevanceResult",
+    "RelevanceWeights",
     "RepositoryIndex",
     "SymbolRecord",
     "build_repository_index",
+    "compile_context",
+    "rank_repository_files",
 ]
