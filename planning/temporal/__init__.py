102af4f79932570c6cd2072fbe6bf2bf24e74f5c
cantidokun <thchumour@gmail.com>
2026-09-19 00:42:38 -0400

    Export Temporal comparison contract version

--- planning/temporal/__init__.py
+++ planning/temporal/__init__.py
@@ -4,6 +4,7 @@ from .admission import AdmissionOutcome, AdmissionState, FromIdentity
 from .ingress import ArrivalValidationError, parse_temporal_observation, parse_temporal_observation_json
 from .canonical import CanonicalValueError, sha256_digest, temporal_canonical_bytes
 from .evaluator import (
+    COMPARISON_CONTRACT_VERSION,
     BoundaryInput,
     BoundaryRefusalInput,
     ComparisonInput,
@@ -39,6 +40,7 @@ __all__ = [
     "BoundaryRefusalInput",
     "CapabilityContract",
     "CanonicalValueError",
+    "COMPARISON_CONTRACT_VERSION",
     "ComparisonInput",
     "DeltaOutcome",
     "DeltaReasonCode",
